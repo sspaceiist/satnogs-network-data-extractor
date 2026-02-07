6d341a8819275e36a50693a7beba058935dc6d65
abhishekpythons <abhishekpythons@gmail.com>
2026-02-07 09:52:03 +0530

    converting 0A

---
+++ README.md
@@ -0,0 +1,3 @@
+level | source | description | fields
+--- | --- | --- | ---
+0A | ksy compiled decoder | Contains diretly converted ccsds packets from kaitai decoder | seconday_header(sh_coarse, sh_fine) + user_data_fields
--- decode.py
+++ decode.py
@@ -1,8 +1,8 @@
 from inspiresat1 import Inspiresat1
+import os
+import pandas as pd
+import sqlite3
 
-pkt = Inspiresat1.from_file(
-    "5468948_28_data_5468948_2022-02-16T04-52-05.bin"
-)
 
 def packet_to_dict(pkt):
     ccsds_pkt = pkt.ax25_frame.payload.ax25_info.ccsds_space_packet
@@ -20,5 +20,15 @@ def packet_to_dict(pkt):
         dict_data[field] = getattr(user_data, field)
     return dict_data
 
-print(packet_to_dict(pkt))
-# print(data.__dict__)
+df = pd.DataFrame()
+
+for filename in os.listdir("/mnt/is1-health/demodulated"):
+    filepath = os.path.join("/mnt/is1-health/demodulated", filename)
+    pkt = Inspiresat1.from_file(filepath)
+    packet_to_dict(pkt)
+    df = pd.concat([df, pd.DataFrame([packet_to_dict(pkt)])], ignore_index=True)
+
+
+conn = sqlite3.connect('level0a.db')
+df.to_sql('packets', conn, if_exists='replace', index=False)
+conn.close()
